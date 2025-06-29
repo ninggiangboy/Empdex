430be4259dd6d002b565e329516b0dab19a068a3
ninggiangboy <ninggiangboy@gmail.com>
2025-06-29 23:07:53 +0700

    init: setup base project and cicd

---
+++ backend/src/test/kotlin/dev/ngb/empdex/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.ngb.empdex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
